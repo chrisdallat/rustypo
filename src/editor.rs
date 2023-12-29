94cfa81d8a59576433aa36a04e1ca7f85cf6c103
Chris Dallat <chr15dallat@hotmail.co.uk>
2023-12-29 15:10:56 +0000

    restructure code to terminal implementation

--- src/editor.rs
+++ src/editor.rs
@@ -1,9 +1,5 @@
 use crate::Terminal;
-use std::io::{self, stdout, Write};
-use termion::raw::IntoRawMode;
-use termion::input::TermRead;
 use termion::event::Key;
-
 
 pub struct Editor {
     should_quit: bool,
@@ -13,8 +9,6 @@ pub struct Editor {
 impl Editor {
 
     pub fn run(&mut self) {
-        let _stdout = stdout().into_raw_mode().unwrap();
-
         loop {
             if let Err(error) = self.refresh_screen() {
                 die(&error)
@@ -36,18 +30,19 @@ impl Editor {
     }
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
-        print!("{}{}", termion::clear::All, termion::cursor::Goto(1,1));
+        Terminal::clear_screen();
+        Terminal::cursor_pos(0,0);
         if self.should_quit {
             println!("Program Exit\r");
         } else {
             self.draw_rows();
-            print!("{}", termion::cursor::Goto(1, 1));
+            Terminal::cursor_pos(0, 0);
         }
-        io::stdout().flush()
+        Terminal::flush()
     }
 
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
-        let pressed_key = read_key()?;
+        let pressed_key = Terminal::read_key()?;
         match pressed_key {
             Key::Ctrl('c') => self.should_quit = true,
             _ => {
@@ -64,16 +59,9 @@ impl Editor {
     }
 
 }
-    
-fn read_key() -> Result<Key, std::io::Error> {
-    loop {
-        if let Some(key) = io::stdin().lock().keys().next() {
-            return key;
-        }
-    }
-}
+
 
 fn die(error: &std::io::Error) {
-    print!("{}", termion::clear::All);
+    Terminal::clear_screen();
     panic!("{}", error);
 }
