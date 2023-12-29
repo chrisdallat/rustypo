bcfb8888051c7d3ff674863f86bcf3caf418e1cf
Chris Dallat <chr15dallat@hotmail.co.uk>
2023-12-29 13:53:29 +0000

    key entry handled with match statement and error handling

--- src/editor.rs
+++ src/editor.rs
@@ -1,4 +1,4 @@
-use std::io::{stdin, stdout};
+use std::io::{self, stdin, stdout};
 use termion::raw::IntoRawMode;
 use termion::input::TermRead;
 use termion::event::Key;
@@ -7,34 +7,41 @@ use termion::event::Key;
 pub struct Editor {}
 
 impl Editor {
+
     pub fn run(&self) {
         let _stdout = stdout().into_raw_mode().unwrap();
 
-        for key in stdin().keys() {
-            match key {
-                Ok(key) => match key {
-                    Key::Char(c) => {
-                        if c.is_control() {
-                            println!("{:?} \r", c as u8); 
-                        } else {
-                            println!("{:?} ({})\r", c as u8, c); 
-                        }
-                    }
-                    Key::Ctrl('c') => break,
-                    _ => println!("{key:?} \r"),
-                },
-                Err(err) => die(&err),
-            }
-        } 
+        loop {
+            if let Err(error) = self.process_keypress() {
+                die(&error)
+            } 
+        }
     }
 
     pub fn default() -> Self {
         Self {}
     }
 
+    fn process_keypress(&self) -> Result<(), std::io::Error> {
+        let pressed_key = read_key()?;
+        match pressed_key {
+            Key::Ctrl('c') => panic!("\r\nProgram Exited!\r\n"),
+            _ => {
+                println!("process_keypress Ok({:?})\r", pressed_key);
+            },
+        }
+        Ok(())
+    }
+
 }
     
-
+fn read_key() -> Result<Key, std::io::Error> {
+    loop {
+        if let Some(key) = io::stdin().lock().keys().next() {
+            return key;
+        }
+    }
+}
 
 fn die(e: &std::io::Error) {
     panic!("{}", e);
