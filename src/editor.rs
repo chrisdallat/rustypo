3849c7eef95c5d7b1c38ec05d826d356d7f30139
Chris Dallat <chr15dallat@hotmail.co.uk>
2023-12-29 16:18:18 +0000

    welcome message and cursor controls implemented

--- src/editor.rs
+++ src/editor.rs
@@ -1,9 +1,17 @@
 use crate::Terminal;
 use termion::event::Key;
 
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+pub struct Position {
+    pub x: usize,
+    pub y: usize,
+}
+
 pub struct Editor {
     should_quit: bool,
     terminal: Terminal,
+    cursor_pos: Position,
 }
 
 impl Editor {
@@ -26,18 +34,21 @@ impl Editor {
         Self {
             should_quit: false,
             terminal: Terminal::default().expect("Failed to initialize Terminal"),
+            cursor_pos: Position { x: 0, y: 0},
         }
     }
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
-        Terminal::clear_screen();
-        Terminal::cursor_pos(0,0);
+        Terminal::cursor_hide();
+        Terminal::cursor_pos(&Position { x: 0, y: 0});
         if self.should_quit {
+            Terminal::clear_screen();
             println!("Program Exit\r");
         } else {
             self.draw_rows();
-            Terminal::cursor_pos(0, 0);
+            Terminal::cursor_pos(&self.cursor_pos);
         }
+        Terminal::cursor_show();
         Terminal::flush()
     }
 
@@ -45,16 +56,66 @@ impl Editor {
         let pressed_key = Terminal::read_key()?;
         match pressed_key {
             Key::Ctrl('c') => self.should_quit = true,
-            _ => {
-                println!("process_keypress Ok({:?})\r", pressed_key);
-            },
+            Key::Up 
+            | Key::Down 
+            | Key::Left 
+            | Key::Right 
+            | Key::PageUp
+            | Key::PageDown
+            | Key::End
+            | Key::Home => self.move_cursor(pressed_key),
+            _ => (),
         }
         Ok(())
     }
 
+    fn move_cursor(&mut self, key: Key) {
+        let Position { mut x, mut y} = self.cursor_pos;
+        let size = self.terminal.size();
+        let height = size.height.saturating_add(1) as usize;
+        let width = size.height.saturating_add(1) as usize;
+        match key {
+            Key::Up => y = y.saturating_sub(1),
+            Key::Down => {
+                if y < height { 
+                    y = y.saturating_add(1);
+                }
+            }
+            Key::Left => x = x.saturating_sub(1),
+            Key::Right => {
+                if x < width {
+                    x = x.saturating_add(1);
+                }
+            }
+            Key::PageUp => y = 0,
+            Key::PageDown => y = height,
+            Key::Home => x = 0,
+            Key::End => x = width,
+            _ => (),
+        }
+        self.cursor_pos = Position {x, y}
+    }
+
+    fn draw_welcome_msg(&self) {
+        let mut welcome_msg = format!("RusTypo -- version {}", VERSION);
+        let width = self.terminal.size().width as usize;
+        let len = welcome_msg.len();
+        let padding = width.saturating_sub(len) / 2;
+        let spaces = " ".repeat(padding.saturating_sub(1));
+        welcome_msg = format!("~{}{}", spaces, welcome_msg);
+        welcome_msg.truncate(width);
+        println!("{}\r", welcome_msg);
+    } 
+
     fn draw_rows(&self) {
-        for _ in 0..self.terminal.size().height {
-            println!("~\r");
+        let height = self.terminal.size().height;
+        for row in 0..height - 1 {
+            Terminal::clear_current_line();
+            if row == height / 3 {
+                self.draw_welcome_msg();
+            } else {
+                println!("~\r");
+            }
         }
     }
 
--- src/terminal.rs
+++ src/terminal.rs
@@ -1,3 +1,4 @@
+use crate::Position;
 use std::io::{self, stdout, Write};
 use termion::event::Key;
 use termion::input::TermRead;
@@ -34,10 +35,26 @@ impl Terminal {
         print!("{}", termion::clear::All);
     }
 
-    pub fn cursor_pos(x: u16, y: u16) {
-        let x = x.saturating_add(1);
-        let y = y.saturating_add(1);
+    #[allow(clippy::cast_possible_truncation)]
+    pub fn cursor_pos(position: &Position) {
+        let Position{mut x, mut y} = position;
+        x = x.saturating_add(1);
+        y = y.saturating_add(1);
+        let x = x as u16;
+        let y = y as u16;
         print!("{}", termion::cursor::Goto(x,y));
+    }
+
+    pub fn cursor_hide() {
+        print!("{}", termion::cursor::Hide);
+    }
+
+    pub fn cursor_show() {
+        print!("{}", termion::cursor::Show);
+    }
+
+    pub fn clear_current_line() {
+        print!("{}", termion::clear::CurrentLine);
     }
 
     pub fn flush() -> Result<(), std::io::Error> {
