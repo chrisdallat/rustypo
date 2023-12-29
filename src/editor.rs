77f21c99b81ab541b8df9eb523f0c318b2d91609
Chris Dallat <chr15dallat@hotmail.co.uk>
2023-12-29 14:29:48 +0000

    add tildes into terminal like vim style

--- src/editor.rs
+++ src/editor.rs
@@ -1,4 +1,4 @@
-use std::io::{self, stdin, stdout};
+use std::io::{self, stdout, Write};
 use termion::raw::IntoRawMode;
 use termion::input::TermRead;
 use termion::event::Key;
@@ -14,18 +14,31 @@ impl Editor {
         let _stdout = stdout().into_raw_mode().unwrap();
 
         loop {
+            if let Err(error) = self.refresh_screen() {
+                die(&error)
+            }
+            if self.should_quit {
+                break;
+            }
             if let Err(error) = self.process_keypress() {
                 die(&error)
             } 
-            if self.should_quit {
-                println!("Program Exited!\r");
-                break;
-            }
         }
     }
 
     pub fn default() -> Self {
         Self {should_quit: false}
+    }
+
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        print!("{}{}", termion::clear::All, termion::cursor::Goto(1,1));
+        if self.should_quit {
+            println!("Program Exit\r");
+        } else {
+            self.draw_rows();
+            print!("{}", termion::cursor::Goto(1, 1));
+        }
+        io::stdout().flush()
     }
 
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
@@ -39,6 +52,12 @@ impl Editor {
         Ok(())
     }
 
+    fn draw_rows(&self) {
+        for _ in 0..24 {
+            println!("~\r");
+        }
+    }
+
 }
     
 fn read_key() -> Result<Key, std::io::Error> {
@@ -50,5 +69,6 @@ fn read_key() -> Result<Key, std::io::Error> {
 }
 
 fn die(error: &std::io::Error) {
+    print!("{}", termion::clear::All);
     panic!("{}", error);
 }
