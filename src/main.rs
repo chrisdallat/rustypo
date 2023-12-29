807d94a10df8c727a5f7c43455cebbce4d07f352
Chris Dallat <chr15dallat@hotmail.co.uk>
2023-12-29 11:59:56 +0000

    added ctrl+q condition to quit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 use std::io::{stdin, stdout, Read};
 use termion::raw::IntoRawMode;
 
+fn to_cntrl_byte(c: char) -> u8 {
+    let byte = c as u8;
+    byte & 0b0001_1111
+}
+
+fn die(e: std::io::Error) {
+    panic!("{}", e);
+}
+
 fn main() {
     println!("Starting Rustypo, Enter Text...");
 
@@ -15,8 +24,7 @@ fn main() {
             println!("{:?} ({})\r", b, c); 
         } 
         
-        if c == 'q'
-        { 
+        if b == to_cntrl_byte('q'){ 
             break;
         }
     }
