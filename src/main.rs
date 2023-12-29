4aedcfb03f2c8abcdc8aa750f7a03de70b759424
Chris Dallat <chr15dallat@hotmail.co.uk>
2023-12-29 11:09:04 +0000

    basic text input stream output to terminal char by char

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
-
-
-
+use std::io::{stdin, stdout, Read};
+use termion::raw::IntoRawMode;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Starting Rustypo, Enter Text...");
+
+    let _stdout = stdout().into_raw_mode().unwrap();
+
+    for b in stdin().bytes() {
+        let c = b.unwrap() as char;
+        println!("{}", c);
+        if c == 'q' 
+        {
+            break;
+        }
+    }
 }
